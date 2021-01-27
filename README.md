873a85b682c37553ff18dd4336e2b8dd2d8e27e5
Tatiana Churkina <tatiana.churkina@mera.com>
2021-01-27 16:21:50 +0300

    Simple form example with usage of formik, antd, yup

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# Getting Started with Create React App
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Simple form example with use of formik, antd and yup
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as Yup from "yup";
+import styled from "styled-components";
+import { Input, Button, Space, Card, Checkbox, Radio, Divider } from "antd";
+import { Formik, Form, Field } from "formik";
+
+const Error = styled.div`
+  color: indianred;
+  text-align: start;
+`;
+const Elem = styled.div`
+  height: 60px;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+          languages: [],
+          jobType: "full-time",
+        }}
+        validationSchema={Yup.object({
+          email: Yup.string()
+            .email("Incorrect email address")
+            .required("Required"),
+          password: Yup.string().required("Required"),
+        })}
+        onSubmit={(data) => {
+          console.log("Submit", data);
+        }}
+      >
+        {({ errors, touched, handleSubmit }) => (
+          <Form>
+            <Space direction="vertical">
+              <Card title="Form" style={{ width: 300 }}>
+                <Elem>
+                  <Field name="email" placeholder="Enter email" as={Input} />
+                  <Error>{touched.email && errors.email}</Error>
+                </Elem>
+                <Elem>
+                  <Field
+                    name="password"
+                    as={Input}
+                    placeholder="Enter password"
+                  />
+                  <Error>{touched.password && errors.password}</Error>
+                </Elem>
+                <Divider />
+                <div>Languages</div>
+                <Space>
+                  <label>
+                    <Field
+                      name="languages"
+                      value="JS"
+                      type="checkbox"
+                      as={Checkbox}
+                    />
+                    JS
+                  </label>
+                  <label>
+                    <Field
+                      name="languages"
+                      value="Java"
+                      type="checkbox"
+                      as={Checkbox}
+                    />
+                    Java
+                  </label>
+                  <label>
+                    <Field
+                      name="languages"
+                      value="Node JS"
+                      type="checkbox"
+                      as={Checkbox}
+                    />
+                    Node JS
+                  </label>
+                  <label>
+                    <Field
+                      name="languages"
+                      value="C++"
+                      type="checkbox"
+                      as={Checkbox}
+                    />
+                    C++
+                  </label>
+                </Space>
+                <Divider />
+                <div>Job Type:</div>
+                <Space direction="vertical">
+                  <label>
+                    <Field
+                      name="jobType"
+                      value="full-time"
+                      type="radio"
+                      as={Radio}
+                    />
+                    Full time employee
+                  </label>
+                  <label>
+                    <Field
+                      name="jobType"
+                      value="part-time"
+                      type="radio"
+                      as={Radio}
+                    />
+                    Part time employee
+                  </label>
+                </Space>
+                <Divider />
+                <Button onClick={handleSubmit} type="primary">
+                  Submit
+                </Button>
+              </Card>
+            </Space>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
